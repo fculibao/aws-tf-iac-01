232bd71b887847883d5913fe89fc5da014e091cc
fculibao <34431067+fculibao@users.noreply.github.com>
2021-09-26 20:14:17 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,11 +5,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myinanpang-keypair01.pem"
-}
-
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
